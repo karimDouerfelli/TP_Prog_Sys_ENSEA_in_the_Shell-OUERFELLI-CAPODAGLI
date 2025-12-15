ec7f4a49d0a23fa89d7c5b1044ce7e036ef9cf92
CaptainJaja <capodagli.janus@outlook.com>
2025-12-15 14:07:49 +0100

    update code

--- Core/Inc/enseash.h
+++ Core/Inc/enseash.h
@@ -1,20 +1,22 @@
+
+
 #ifndef ENSEASH_H
 #define ENSEASH_H
+
+// dependencies
 
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <time.h>
-#include "enseash.h"
 
 #define BUF_SIZE 1024
 #define WELCOME_MSG "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n"
 #define PROMPT "enseash % "
 
-// Prototypes
+// Declarations
 void display_prompt();
 void read_command(char *buf);
-// Ajoutez vos autres prototypes ici au fur et à mesure
 
 #endif
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -7,10 +7,7 @@ Author :
 */
 
 // Dependencies 
-#include <stdio.h>
-#include <stdlib.h>
-#include <syscall.h>
-#include <assert.h>
+#include "enseash.h"
 
 
 
@@ -20,43 +17,9 @@ int main(void) {
 
     char buffer[BUF_SIZE];
 
-    // Q2: REPL Loop (Read-Eval-Print Loop) 
-    while (1) {
-        // Display prompt
-        display_message(PROMPT);
+    
 
-        // Q2a: Read command
-        // Note: Reset buffer to ensure clean read
-        memset(buffer, 0, BUF_SIZE);
-        ssize_t bytes_read = read(STDIN_FILENO, buffer, BUF_SIZE - 1);
-
-        // Q3: Handle Ctrl+D (EOF) which returns 0 bytes [cite: 41]
-        if (bytes_read == 0) {
-            display_message("\nBye bye...\n");
-            break;
-        }
-        
-        // Handle read errors
-        if (bytes_read < 0) {
-            const char *err_msg = "Error reading input.\n";
-            write(STDERR_FILENO, err_msg, strlen(err_msg));
-            exit(EXIT_FAILURE);
-        }
-
-        // Remove the newline character '\n' from the input
-        buffer[strcspn(buffer, "\n")] = '\0';
-
-        // Q3: Handle "exit" command [cite: 41]
-        if (strncmp(buffer, EXIT_CMD, strlen(EXIT_CMD)) == 0 && strlen(buffer) == strlen(EXIT_CMD)) {
-             display_message("Bye bye...\n");
-             break;
-        }
-
-        // Q2b: Execute simple command
-        execute_command(buffer);
-    }
-
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 
